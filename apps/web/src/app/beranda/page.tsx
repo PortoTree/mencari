4edb317089013db3f5e5f3968ae2a04ad1540b5b
Pusat-bgl <pamfaiz64@gmail.com>
2026-09-16 00:16:17 +0700

    MENCARI UPDATE 09

--- apps/web/src/app/beranda/page.tsx
+++ apps/web/src/app/beranda/page.tsx
@@ -117,11 +117,12 @@ export default function Beranda() {
           </div>
 
           {/* Vertical Separator */}
-          <div className="w-[1px] h-6 bg-gray-300 mx-1"></div>
+          <div className="w-[1px] h-6 bg-gray-300 dark:bg-[#3E4042] mx-1"></div>
 
-          <div className="relative group">
-            <button className="w-10 h-10 rounded-full bg-[#E4E6EB] dark:bg-[#3A3B3C] flex items-center justify-center text-black dark:text-[#E4E6EB] hover:bg-[#F3F2EF] dark:hover:bg-[#18191A] transition-colors overflow-hidden">
-              <img src="/menu.svg" alt="Menu" className="w-[22px] h-[22px] object-contain" />
+          {/* MENU ICON - NO CIRCLE */}
+          <div className="relative group flex items-center justify-center mr-2 ml-1">
+            <button className="flex items-center justify-center transition-transform hover:scale-105 active:scale-95">
+              <img src="/menu.svg" alt="Menu" className="w-[28px] h-[28px] object-contain" />
             </button>
             <div className="absolute top-12 left-1/2 -translate-x-1/2 px-3 py-1.5 bg-black/80 text-white text-[13px] rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-150 pointer-events-none whitespace-nowrap z-[60]">
               Menu
